d51fc351d1d75200cf8147222cc524f720a1bffb
FlynntKnapp <bruce.stull@gmail.com>
2023-12-01 13:25:25 -0500

    Add 'Car' class for examing json.dumps output.

---
+++ models.py
@@ -0,0 +1,17 @@
+class Car:
+    def __init__(self, name, model, year):
+        self.name = name
+        self.model = model
+        self.year = year
+
+    def get_name(self):
+        return self.name
+
+    def get_model(self):
+        return self.model
+
+    def get_year(self):
+        return self.year
+
+    def __str__(self):
+        return f"{self.name} {self.model} {self.year}"
