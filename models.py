e3e47cb77c8e0e7d5538d612171661bed7289cf0
FlynntKnapp <bruce.stull@gmail.com>
2023-12-09 15:02:27 -0500

    Add class docstring, change 'name' to 'brand'.

--- models.py
+++ models.py
@@ -1,11 +1,20 @@
 class Car:
-    def __init__(self, name, model, year):
-        self.name = name
+    """
+    Class to represent a car.
+
+    Attributes:
+        brand (str): The brand of the car.
+        model (str): The model of the car.
+        year (int): The year the car was made.
+    """
+
+    def __init__(self, brand, model, year):
+        self.brand = brand
         self.model = model
         self.year = year
 
-    def get_name(self):
-        return self.name
+    def get_brand(self):
+        return self.brand
 
     def get_model(self):
         return self.model
@@ -14,4 +23,4 @@ class Car:
         return self.year
 
     def __str__(self):
-        return f"{self.name} : {self.model} - {self.year}"
+        return f"{self.brand} : {self.model} - {self.year}"
